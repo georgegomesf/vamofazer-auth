aad4125e4c39c8a6900e75218d6522bcd68904af
George Gomes <georgegomesf@gmail.com>
2026-03-18 20:06:15 -0300

    feat: add API endpoint to retrieve a single project by ID.

---
+++ src/app/api/projects/[id]/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(
+    request: Request,
+    { params }: { params: { id: string } }
+) {
+    try {
+        const id = (await params).id;
+        const project = await prisma.project.findUnique({
+            where: { id },
+            select: {
+                id: true,
+                name: true,
+                logoUrl: true,
+            }
+        });
+
+        if (!project) {
+            return NextResponse.json({ error: "Project not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(project);
+    } catch (error) {
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
